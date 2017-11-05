66d5f9de3582853e266aa7d59d9adaf6207f1730
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-11-05 20:52:44 +0100

    Added potential in plot for Hydrogen atom solution.

--- examples/test_radial.py
+++ examples/test_radial.py
@@ -23,7 +23,7 @@ print "Energy: %.8f +/- %.8f" % (energy, energy_error)
 
 
 from bokeh.plotting import figure, output_file, save
-from bokeh.models import Label
+from bokeh.models import Label, Range1d
 output_file("test_radial.html", title="QuantumMC test result")
 p = figure(title="QuantumMC test result", x_axis_label='r', y_axis_label='r R(r)', width = 800, height = 800)
 
@@ -32,11 +32,14 @@ psi_a = np.asarray(psi)
 
 p.line(r_a, 2*r_a*np.exp(-r_a), legend="r R(r) for H", line_width=2, line_color="blue")
 p.circle(r_a, psi_a, legend="r R(r)", line_width=2, line_color="red")
-energy = Label(x=70, y=750, x_units='screen', y_units='screen',
-                 text='Energy = %.5f +/- %.5f' % (energy, energy_error), render_mode='css',
+p.line(r_a, V(r_a), legend="Effective potential for l = 0", line_width=2, line_color="green")
+energy = Label(x=70, y=700, x_units='screen', y_units='screen',
+                 text='Energy = %.8f +/- %.8f Hartree' % (energy, energy_error), render_mode='css',
                  border_line_color='black', border_line_alpha=0.0,
                  background_fill_color='white', background_fill_alpha=0.0)
 p.add_layout(energy)
+p.y_range = Range1d(np.min(V(r_a)), np.max(psi_a)*1.5)
+p.x_range = Range1d(0, 8)
 
 save(p, "test_radial.html", title = "QuantumMC test result")
 
